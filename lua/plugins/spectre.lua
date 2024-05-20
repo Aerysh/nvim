97c2083ef2dac6db29b2472ebbab60337325da5a
Ahmad Azwar Annas <ahmadazw2@gmail.com>
2024-05-20 21:06:44 +0700

    feat: add my usual plugins

---
+++ lua/plugins/spectre.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "nvim-pack/nvim-spectre",
+    cmd = "Spectre",
+    opts = { open_cmd = "noswapfile vnew" },
+    keys = {
+      {
+        "<leader>sr",
+        function()
+          require("spectre").open()
+        end,
+        desc = "Replace in files",
+      },
+      {
+        "<leader>sp",
+        ":lua require('spectre').open()<CR>",
+        desc = "Replace in files (Root dir)",
+      },
+      -- Search current word
+      {
+        "<leader>sP",
+        ":lua require('spectre').open_visual({ select_word = true })<CR>",
+        desc = "Replace current word (Root dir)",
+      },
+      -- Open search with select word in visual mode
+      {
+        "<leader>sr",
+        ":lua require('spectre').open_visual()<CR>",
+        mode = "v",
+        silent = true,
+        desc = "Replace current word (Root dir)",
+      },
+      -- Search on current file
+      {
+        "<leader>sf",
+        ":lua require('spectre').open_file_search({select_word=true})<CR>",
+        desc = "Replace in current file",
+      },
+    },
+  },
+}
